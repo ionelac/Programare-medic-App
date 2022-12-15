ff10ff43f3af756deff81702d3bdfe97d31e08e6
ionelac <fcomsa40@gmail.com>
2022-12-15 09:50:39 +0200

    Add project files.

---
+++ Programare medic App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Programare_medic_App;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ Programare medic App/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Programare_medic_App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
